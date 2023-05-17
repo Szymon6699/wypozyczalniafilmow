9ed2d9ade8b05caec7689c50955aca3101e2122a
Szymon6699 <113582482+Szymon6699@users.noreply.github.com>
2023-05-17 10:11:42 +0200

    1
    1

---
+++ wypozyczalniafilmow/video.php
@@ -0,0 +1,59 @@
+<html>
+    
+    <head>
+        <title>Video On Demand</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styl4.css">
+    
+    </head>
+    
+    <body>
+        <div class="b1">
+        
+            <h1>Internetowa wypożyczalnia filmów</h1>
+            
+        </div>
+        
+        <div class="b2">
+            <table>
+                <tr>
+                    <td>Kryminał</td> <td>Horror</td> <td>Przygodowy</td>
+                </tr>
+                <tr>
+                    <td>20</td> <td>30</td> <td>20</td>
+                </tr>
+            </table>
+        
+        </div>
+        
+        <div class="bp">
+            
+            <h3>Polecamy</h3>
+            <?php
+            
+            ?>
+            
+        </div>
+        
+        <div class="bff">
+            
+            <h3>Filmy fantastyczne</h3>
+            <?php
+            
+            ?>
+        
+        </div>
+        
+        <div class="s1">
+        
+            <form method="post">
+                
+                Usuń film nr.:<input type="number">
+                
+            </form>
+            
+        </div>
+        
+    </body>
+
+</html>
